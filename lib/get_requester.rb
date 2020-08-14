59a853aaaf9b340df48863f729695beb61e78b9e
George Talnagi <gtalnagi@yahoo.com>
2020-08-14 22:54:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,23 +1,19 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
-
-
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
+response.body
 
 class GetRequester
 
-
-
- 
   def initialize(url)
     
   end 
  
   def get_response_body
    
-  
-
-
   end 
   
   def parse_json
