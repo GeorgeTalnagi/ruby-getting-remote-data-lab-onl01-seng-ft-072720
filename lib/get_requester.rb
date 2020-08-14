1263788f1be46829c49c1ad16aa646de854d6744
George Talnagi <gtalnagi@yahoo.com>
2020-08-14 22:48:41 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,14 +14,14 @@ class GetRequester
   end 
  
   def get_response_body
- 
-  end 
-  
-  def parse_json
     url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     response.body
+  end 
+  
+  def parse_json
+   
   end 
  
 end 
